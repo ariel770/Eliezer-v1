2948382f9c068b2d505c82d3d758be9e79742f12
pinjas <pinjas31@gmail.com>
2020-09-02 11:46:36 +0300

    change port to 0000

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ useCreateIndex:true}).then(()=>{
 
 });
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 
 
-app.listen(3000, "localhost", function (req, res) {
 
-    console.log("server is ready  ... ")
-})
+// app.listen(3000, "localhost", function (req, res) {
+
+//     console.log("server is ready  ... ")
+// })
